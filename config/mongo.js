61620f4c2db2e5387720c6b14ff66976cf0355a5
HaiNgo <haingo6394@gmail.com>
2018-01-08 16:35:33 +0700

    paginator for fusion-mongo

--- config/mongo.js
+++ config/mongo.js
@@ -1,2 +1,3 @@
 // Native mongodb client configuration
-module.exports = process.env.MONGO_DB || 'mongodb://localhost:27017/your_mongodb';
+exports.host     = process.env.MONGO_HOST || 'mongodb://localhost:27017';
+exports.database = process.env.MONGO_DB || 'your_mongodb';
