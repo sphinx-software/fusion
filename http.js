2d700e958baa361f209accf3d46a1527065de101
NgocTo <tongoc92@gmail.com>
2018-01-25 17:17:20 +0700

    add console and http bootstrap

--- http.js
+++ http.js
@@ -1,16 +1,24 @@
+import lodash from 'lodash';
 import config from 'config';
 import bootstrap from 'bootstrap';
-import Container from '@sphinx-software/fusion/container';
+import Container from '@sphinx-software/container';
 import {EventEmitter} from 'events';
-
-
 
 (async () => {
     let container  = await bootstrap(config, new Container(new EventEmitter()));
     let httpKernel = await container.make('http.kernel');
     let router     = await container.make('http.router');
-    
+
+    config.providers.forEach((provider) => provider.register(container));
+
     httpKernel
+        .use(async (context, next) => {
+            let httpBootedProviders = config.providers.filter(provider => lodash.isFunction(provider.bootHttp));
+            for (let index = 0; index < httpBootedProviders.length; index++) {
+                await httpBootedProviders[index].bootHttp(context);
+            }
+            await next();
+        })
         .use(router.routes())
         .use(router.allowedMethods())
     ;
