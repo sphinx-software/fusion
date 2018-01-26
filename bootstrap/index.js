e31cfb5da3209d8fa71210273df318e60c155a84
rikky <son.levuthai@gmail.com>
2018-01-26 19:31:56 +0700

    update demo for the skeleton in v1

--- bootstrap/index.js
+++
@@ -1,6 +0,0 @@
-import containerBootstrap from './container.bootstrap';
-
-export default async function (config, container) {
-    await containerBootstrap(config, container);
-    return container;
-}
