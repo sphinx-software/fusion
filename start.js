56810ab0f121c3b3a00168cb9943c9a2573519ee
rikky <son.levuthai@gmail.com>
2018-02-06 02:39:51 +0700

    using build version of the framework

--- start.js
+++ start.js
@@ -1,7 +1,5 @@
 const path = require('path');
 require('app-module-path').addPath(path.normalize(path.join(__dirname, 'src')));
 require('reflect-metadata');
-require('babel-register')({
-    ignore: /node_modules\/(?!@sphinx-software)/
-});
+require('babel-register');
 require('./http.bootstrap');
