2306ded14503a8901a8a05644f2c9852cbd8dd2b
rikky <son.levuthai@gmail.com>
2018-02-06 22:54:18 +0700

    update the test feature with wave-function

---
+++ test/Tests/IndexRouteTestSuite.js
@@ -0,0 +1,23 @@
+import {testSuite, testCase} from "WaveFunction";
+import TestSuite from "../TestSuite";
+import http from 'http';
+import {JSDOM} from 'jsdom';
+
+import Koa from 'koa';
+
+@testSuite()
+export default class IndexRouteTestSuite extends TestSuite {
+
+    @testCase()
+    async testIfIndexIsOkay() {
+        // Using chai-http
+        // For mor usage, see http://chaijs.com/plugins/chai-http/
+        let response = await this.dispatchHttp().get('/');
+
+        this.chai.expect(response).to.have.status(200);
+
+        // Assert the title content
+        let dom = new JSDOM(response.text);
+        this.chai.expect(dom.window.document.querySelector('h1').textContent).to.be.equal('Hi! from Fusion');
+    }
+}
---
+++ test/Tests/index.js
@@ -0,0 +1,1 @@
+export IndexRouteTestSuite from "./IndexRouteTestSuite";
