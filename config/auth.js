2e122039421462acca514f59346c758502f30761
NgocTo <tongoc92@gmail.com>
2018-02-23 09:36:41 +0700

    update auth configuration

--- config/auth.js
+++ config/auth.js
@@ -1,26 +1,6 @@
 export default {
 
-    "default": "session",
-    "adapter": "database",
-
-    "adapters": {
-        "database": {
-            "credentialTable": "credentials",
-            "usernameField": "username",
-            "passwordField": "password"
-        },
-        "mongodb": {
-            "credentialCollection": "credentials",
-            "usernameField": "username",
-            "passwordField": "password"
-        }
-    },
-
     "session": {
         "sessionAuthKey": "sphinx-software"
-    },
-
-    "token": {
-
     }
 }
